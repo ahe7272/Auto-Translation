e3e4604522209f37a60881727b411033fc475b4f
Heeone Lee <55167422+ahe7272@users.noreply.github.com>
2021-03-16 21:51:06 +0900

    Add files via upload

--- googleTranslator.py
+++ googleTranslator.py
@@ -5,17 +5,6 @@ import pandas as pd
 from pandas import DataFrame
 from selenium.webdriver.common.keys import Keys
 
-Multi_lines = []
-
-def indexer(file):
-    with open(file, 'r') as f:
-        for i in f.read().split('\n'):
-            Multi_lines.append(i)
-         
-indexer(input('어떤 파일을 번역할까요?'))
-
-g_translated = []
-
 def googleTranslator(language):
     urls = {
         'ar' : 'https://translate.google.co.kr/?sl=auto&tl=ar&op=translate',
@@ -33,35 +22,8 @@ def googleTranslator(language):
     }
     driver  = webdriver.Chrome(executable_path='/Users/HeewonLee/Desktop/COMPUTER/CODING/chromedriver')
     driver.get(urls[language])
-    
-    time.sleep(3)    
-    
-    trlen = input('몇 줄 씩 번역할까요?(숫자만 입력해 주세요)')    
-    try:
-        while Multi_lines != []:
-            google_input = driver.find_element_by_xpath('//*[@id="yDmH0d"]/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[2]/c-wiz[1]/span/span/div/textarea')
-            google_input.send_keys("\n".join(Multi_lines[:int(trlen)]))
-            time.sleep(3)
-            translated = driver.find_element_by_xpath('//*[@id="yDmH0d"]/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[2]/c-wiz[2]/div[5]/div/div[3]').get_attribute('data-text').split('\n')
-            g_translated.append(translated)
-            time.sleep(3)
-            driver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + Keys.HOME)
-            time.sleep(1)
-            driver.find_element_by_xpath('//*[@id="yDmH0d"]/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[2]/c-wiz[1]/div[2]').click()
-            del Multi_lines[:int(trlen)] 
-  
-        driver.close()  
-        df = DataFrame(g_translated).transpose()
-        df.to_excel('Gtranslated.xlsx')      
-    
-    except:
-        print('뭔가 잘못되었어!')
-        df = DataFrame(g_translated).transpose()
-        df.to_excel('Gtranslated.xlsx') 
-        driver.close()
-    
-googleTranslator(input('어떤 언어로 번역할까요? (입력 가능 언어: ar, de, en, fr, it, ja, ko, mn, ru, sv, vi, zh)'))
-  
+
+    return driver  
+
 time.sleep(3)
         
-

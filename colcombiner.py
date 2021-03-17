ba6a9d5e7423d15882e86dafedb1bc4f5a70c183
Heeone Lee <ahe7272@naver.com>
2021-03-17 18:28:58 +0900

    Added user input for file name to save as

--- colcombiner.py
+++ colcombiner.py
@@ -3,6 +3,7 @@ import openpyxl
 wb = openpyxl.load_workbook(input('어떤 파일을 실행할까요?'))
 sheet1 = wb.active 
 
+combined = []
 colname = ['B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 alphabets = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 for x in alphabets:
@@ -17,4 +18,8 @@ for i in colname:
   for row in range(2,52):
     if sheet1[i+str(row)].value == None:
         break
-    print(sheet1[i+str(row)].value)
+    combined.append(sheet1[i+str(row)].value)
+
+file = open(input('무슨 이름으로 파일을 저장할까요?(확장명과 같이 입력해 주세요)'), "w") 
+file.write(str('\n'.join(combined))) 
+file.close()
